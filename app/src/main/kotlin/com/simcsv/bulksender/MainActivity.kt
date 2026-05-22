c43bf48e9e2fd9318d7ecf83fafd41b1fbe338a2
johnnycvv <stoiccity@adexec.com>
2026-05-22 13:36:15 +0100

    Update MainActivity.kt

--- app/src/main/kotlin/com/simcsv/bulksender/MainActivity.kt
+++ app/src/main/kotlin/com/simcsv/bulksender/MainActivity.kt
@@ -2,10 +2,10 @@ package com.simcsv.bulksender
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.simcsv.bulksender.databinding.ActivityMainBinding
 
@@ -22,18 +22,34 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        val appBarConfig = AppBarConfiguration(
-            setOf(
-                R.id.dashboardFragment,
-                R.id.csvImportFragment,
-                R.id.logsFragment,
-                R.id.settingsFragment
-            )
+        val topLevelIds = setOf(
+            R.id.dashboardFragment,
+            R.id.csvImportFragment,
+            R.id.logsFragment,
+            R.id.settingsFragment
         )
 
+        val appBarConfig = AppBarConfiguration(topLevelIds)
         setSupportActionBar(binding.toolbar)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig)
-        binding.bottomNav.setupWithNavController(navController)
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            val currentId = navController.currentDestination?.id
+            if (currentId == item.itemId) return@setOnItemSelectedListener true
+
+            val options = NavOptions.Builder()
+                .setPopUpTo(R.id.dashboardFragment, inclusive = false, saveState = false)
+                .setLaunchSingleTop(true)
+                .setRestoreState(false)
+                .build()
+
+            try {
+                navController.navigate(item.itemId, null, options)
+                true
+            } catch (_: Exception) {
+                false
+            }
+        }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val noNavDest = setOf(R.id.previewFragment, R.id.progressFragment)
@@ -41,6 +57,9 @@ class MainActivity : AppCompatActivity() {
                 android.view.View.GONE
             } else {
                 android.view.View.VISIBLE
+            }
+            if (destination.id in topLevelIds) {
+                binding.bottomNav.menu.findItem(destination.id)?.isChecked = true
             }
         }
     }
